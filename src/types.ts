501f51669d1030ce6f5947a6cb5b6d1ad8671eac
escapedcat <github@htmlcss.de>
2022-10-19 17:52:58 +0200

    refactor: use budget.success event directly #meta-15

--- src/types.ts
+++ src/types.ts
@@ -544,7 +544,7 @@ export interface Allowance extends Omit<DbAllowance, "id"> {
 
 export enum AlbyEventType {
   "AUTH" = "AUTH",
-  "BUDGET" = "BUDGET", //unused
+  "BUDGET" = "BUDGET",
   "CHANNEL" = "CHANNEL", //unused
   "INVOICE" = "INVOICE",
   "PERMISSION" = "PERMISSION", //unused
@@ -567,6 +567,7 @@ export interface AlbyEvent extends Omit<DbAlbyEvent, "id"> {
 export interface AlbyEventBudgetUpdateDetails {
   type: "update";
   allowanceId: Allowance["id"];
+  event: AlbyEventType.BUDGET;
 }
 
 export interface AlbyEventInvoiceDetails {
